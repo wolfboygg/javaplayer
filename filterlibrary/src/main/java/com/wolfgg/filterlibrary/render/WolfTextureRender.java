2d83f8ac7629a56bf0199776144dd93829ce87cb
guolei <guolei@do-global.com>
2019-10-30 20:11:53 +0800

    change[]:添加绘制使用vbo功能

--- filterlibrary/src/main/java/com/wolfgg/filterlibrary/render/WolfTextureRender.java
+++ filterlibrary/src/main/java/com/wolfgg/filterlibrary/render/WolfTextureRender.java
@@ -18,6 +18,15 @@ import java.nio.FloatBuffer;
  * 绘制纹理需要进行加载shader，创建顶点和纹理坐标
  * <p>
  * 先实现功能，最后在进行结构的抽取
+ * <p>
+ * <p>
+ * 添加vbo功能和fbo功能
+ * VBO
+ * 1. 创建VBO得到vboID
+ * 2. 根据id绑定vbo
+ * 3. 分配vbo需要的缓存大小
+ * 4. 为vbo设置顶点数据的值
+ * 5. 解绑vbo
  */
 
 public class WolfTextureRender implements WolfEGLSurfaceView.WolfGLRender {
@@ -49,6 +58,8 @@ public class WolfTextureRender implements WolfEGLSurfaceView.WolfGLRender {
     private int fPosition; // 纹理坐标
     private int sampler; // 纹理
 
+    private int vboId;
+
     private int imgTextureId;
 
     public WolfTextureRender(Context context) {
@@ -78,6 +89,8 @@ public class WolfTextureRender implements WolfEGLSurfaceView.WolfGLRender {
         fPosition = GLES20.glGetAttribLocation(mProgram, "f_Position");
         sampler = GLES20.glGetUniformLocation(mProgram, "sTexture");
 
+        createVBO();
+
         imgTextureId = ShaderUtils.loadTexture(mContext, R.drawable.androids);
 
     }
@@ -102,17 +115,52 @@ public class WolfTextureRender implements WolfEGLSurfaceView.WolfGLRender {
         GLES20.glUseProgram(mProgram);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, imgTextureId);
 
+        drawUserVBO();
+
+        //使用绘制三角形的方式进行绘制
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+    }
+
+    private void drawNormal() {
         // 然后进行绘制
         GLES20.glEnableVertexAttribArray(vPosition);
         GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, mVertexFloatBuffer);
 
         GLES20.glEnableVertexAttribArray(fPosition);
         GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, mFragmentFloatBuffer);
-
-        //使用绘制三角形的方式进行绘制
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
-
-
     }
+
+
+    /**
+     * 创建vbo
+     */
+    private void createVBO() {
+        // 1.创建vbo
+        int[] vbos = new int[1];
+        GLES20.glGenBuffers(1, vbos, 0);
+        vboId = vbos[0];
+        // 2. 绑定vbo
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+        // 3. 分配大小
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertex.length * 4 + fragment.length * 4, null, GLES20.GL_STREAM_DRAW);
+        // 4.设置数据
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, 0, vertex.length * 4, mVertexFloatBuffer);
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, vertex.length * 4, fragment.length * 4, mFragmentFloatBuffer);
+        // 5. 解除绑定
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
+    private void drawUserVBO() {
+        // 绑定vbo
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+        // 设置数据
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8, 0); // 第二个参数表示的去几个点每次
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8, vertex.length * 4);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
+
 }
