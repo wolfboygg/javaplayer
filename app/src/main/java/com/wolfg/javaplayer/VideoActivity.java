62bd8e5212cecdb5cd78054455109d64135e4165
guolei <guolei@do-global.com>
2019-11-18 15:32:56 +0800

    change[]:添加声音视频合成功能

--- app/src/main/java/com/wolfg/javaplayer/VideoActivity.java
+++ app/src/main/java/com/wolfg/javaplayer/VideoActivity.java
@@ -2,7 +2,6 @@ package com.wolfg.javaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaFormat;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -13,6 +12,10 @@ import com.wolfgg.cameralibrary.video.WolfBaseMediaEncoder;
 import com.wolfgg.cameralibrary.video.WolfMediaEncodec;
 import com.wolfgg.filterlibrary.utils.LogHelper;
 import com.wolfgg.filterlibrary.utils.UiUtils;
+import com.ywl5320.libmusic.WlMusic;
+import com.ywl5320.listener.OnCompleteListener;
+import com.ywl5320.listener.OnPreparedListener;
+import com.ywl5320.listener.OnShowPcmDataListener;
 
 public class VideoActivity extends AppCompatActivity implements WolfBaseMediaEncoder.OnMediaInfoListener {
 
@@ -20,6 +23,8 @@ public class VideoActivity extends AppCompatActivity implements WolfBaseMediaEnc
 
     private WolfCameraView mWolfCameraView;
     private Button mButton;
+
+    private WlMusic mWlMusic;
 
     private WolfMediaEncodec mWolfMediaEncodec;
 
@@ -29,23 +34,64 @@ public class VideoActivity extends AppCompatActivity implements WolfBaseMediaEnc
         setContentView(R.layout.activity_video);
         mWolfCameraView = findViewById(R.id.cameraview);
         mButton = findViewById(R.id.btn_record);
+
+        mWlMusic = WlMusic.getInstance();
+        mWlMusic.setCallBackPcmData(true);
+        mWlMusic.setOnPreparedListener(new OnPreparedListener() {
+            @Override
+            public void onPrepared() {
+                mWlMusic.playCutAudio(1, 30);
+            }
+        });
+
+        mWlMusic.setOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete() {
+                if (mWolfMediaEncodec != null) {
+                    mWolfMediaEncodec.stopRecode();
+                    mWolfMediaEncodec = null;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mButton.setText("开始录制");
+                        }
+                    });
+                }
+            }
+        });
+
+        mWlMusic.setOnShowPcmDataListener(new OnShowPcmDataListener() {
+            @Override
+            public void onPcmInfo(int samplerate, int bit, int channels) {
+                LogHelper.d(TAG, "textureid is " + mWolfCameraView.getTextureId());
+                mWolfMediaEncodec = new WolfMediaEncodec(VideoActivity.this, mWolfCameraView.getTextureId());
+                mWolfMediaEncodec.setOnMediaInfoListener(VideoActivity.this);
+                mWolfMediaEncodec.initEncodec(mWolfCameraView.getEGLContext(),
+                        Environment.getExternalStorageDirectory().getAbsolutePath() + "/wl_live_pusher.mp4",
+                        UiUtils.getScreenWidthPixels(VideoActivity.this), UiUtils.getScreenHeightPixels(VideoActivity.this), samplerate, channels);
+                mWolfMediaEncodec.startRecode();
+            }
+
+            @Override
+            public void onPcmData(byte[] pcmdata, int size, long clock) {
+                if (mWolfMediaEncodec != null) {
+                    mWolfMediaEncodec.putPCMData(pcmdata, size);
+                }
+            }
+        });
     }
 
     public void record(View view) {
         if (mWolfMediaEncodec == null) {
             LogHelper.i(TAG, "textureId is = " + mWolfCameraView.getTextureId());
-            mWolfMediaEncodec = new WolfMediaEncodec(this, mWolfCameraView.getTextureId());
-            mWolfMediaEncodec.setOnMediaInfoListener(this);
-            mWolfMediaEncodec.initEncodec(mWolfCameraView.getEGLContext(),
-                    Environment.getExternalStorageDirectory().getAbsolutePath() + "/wl_live_pusher.mp4",
-                    MediaFormat.MIMETYPE_VIDEO_AVC, UiUtils.getScreenWidthPixels(this), UiUtils.getScreenHeightPixels(this)
-            );
-            mWolfMediaEncodec.startRecode();
+            mWlMusic.setSource(Environment.getExternalStorageDirectory().getAbsolutePath() + "/cd.mp3");// 这里不能直接传入aac格式的文件 wlMusic是有问题的
+            mWlMusic.prePared();
             mButton.setText("正在录制");
         } else {
             mWolfMediaEncodec.stopRecode();
             mButton.setText("开始录制");
             mWolfMediaEncodec = null;
+            mWlMusic.stop();
         }
     }
 
--- cameralibrary/src/main/java/com/wolfgg/cameralibrary/video/WolfBaseMediaEncoder.java
+++ cameralibrary/src/main/java/com/wolfgg/cameralibrary/video/WolfBaseMediaEncoder.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
 import android.media.MediaFormat;
+import android.media.MediaMetadata;
 import android.media.MediaMuxer;
 import android.view.Surface;
 
@@ -34,7 +35,19 @@ public abstract class WolfBaseMediaEncoder {
     private MediaCodec mVideoEncodec;
     private MediaFormat mVideoFormat;
     private MediaCodec.BufferInfo mVideoBufferinfo;
+
+    // 声音录制功能
+    private MediaCodec mAudioEncodec;
+    private MediaFormat mAudioFormat;
+    private MediaCodec.BufferInfo mAudioBufferInfo;
+
+    private long audioPts = 0; // 这个pts需要手动计算，传入编码器
+    private int sampleRate;
+
     private MediaMuxer mMediaMuxer;
+    private boolean encodecStart;
+    private boolean audioExit;
+    private boolean videoExit;
 
 
     public final static int RENDERMODE_WHEN_DIRTY = 0;
@@ -48,6 +61,7 @@ public abstract class WolfBaseMediaEncoder {
 
     private WolfEGLMediaThread mWolfEGLMediaThread;
     private VideoEncodecThread mVideoEncodecThread;
+    private AudioEncodecThread mAudioEncodecThread;
 
 
     public WolfBaseMediaEncoder(Context context) {
@@ -69,18 +83,19 @@ public abstract class WolfBaseMediaEncoder {
         mOnMediaInfoListener = onMediaInfoListener;
     }
 
-    public void initEncodec(EGLContext eglContext, String savePath, String mimeType, int width, int height) {
-        LogHelper.i(TAG, "EGLContext is = " +eglContext);
+    public void initEncodec(EGLContext eglContext, String savePath, int width, int height, int sampleRate, int channelCount) {
+        LogHelper.i(TAG, "EGLContext is = " + eglContext);
         this.mWidth = width;
         mHeight = height;
         mEGLContext = eglContext;
-        initMeidaEncodec(savePath, mimeType, width, height);
+        initMeidaEncodec(savePath, width, height, sampleRate, channelCount);
     }
 
-    private void initMeidaEncodec(String savePath, String mimeType, int width, int height) {
+    private void initMeidaEncodec(String savePath, int width, int height, int sampleRate, int channelCount) {
         try {
             mMediaMuxer = new MediaMuxer(savePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
-            initVideoEncodec(mimeType, width, height);
+            initVideoEncodec(MediaFormat.MIMETYPE_VIDEO_AVC, width, height);
+            initAudioEncodec(MediaFormat.MIMETYPE_AUDIO_AAC, sampleRate, channelCount);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -110,30 +125,79 @@ public abstract class WolfBaseMediaEncoder {
         }
     }
 
+    /**
+     * 初始化音频编码器
+     *
+     * @param mimeType
+     * @param sampleRate
+     * @param channelCount
+     */
+    private void initAudioEncodec(String mimeType, int sampleRate, int channelCount) {
+        try {
+            this.sampleRate = sampleRate;
+            mAudioBufferInfo = new MediaCodec.BufferInfo();
+            mAudioFormat = MediaFormat.createAudioFormat(mimeType, sampleRate, channelCount);
+            mAudioFormat.setInteger(MediaFormat.KEY_BIT_RATE, 96000);// 码率
+            mAudioFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
+            mAudioFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 4096);
+
+            mAudioEncodec = MediaCodec.createEncoderByType(mimeType);
+            mAudioEncodec.configure(mAudioFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            mAudioBufferInfo = null;
+            mAudioFormat = null;
+            mAudioEncodec = null;
+        }
+    }
+
 
     public void startRecode() {
         if (mSurface != null && mEGLContext != null) {
+
+            audioPts = 0;
+            audioExit = false;
+            videoExit = false;
+            encodecStart = false;
+
             mWolfEGLMediaThread = new WolfEGLMediaThread(new WeakReference<WolfBaseMediaEncoder>(this));
             mVideoEncodecThread = new VideoEncodecThread(new WeakReference<WolfBaseMediaEncoder>(this));
+            mAudioEncodecThread = new AudioEncodecThread(new WeakReference<WolfBaseMediaEncoder>(this));
 
             mWolfEGLMediaThread.isCreate = true;
             mWolfEGLMediaThread.isChange = true;
 
             mWolfEGLMediaThread.start();
             mVideoEncodecThread.start();
+            mAudioEncodecThread.start();
         }
     }
 
     public void stopRecode() {
-        if (mWolfEGLMediaThread != null && mVideoEncodecThread != null) {
+        if (mWolfEGLMediaThread != null && mVideoEncodecThread != null && mAudioEncodecThread != null) {
             mVideoEncodecThread.exit();// 先停止编码线程
+            mAudioEncodecThread.exit();
             mWolfEGLMediaThread.onDestroy();
             mVideoEncodecThread = null;
+            mAudioEncodecThread = null;
             mWolfEGLMediaThread = null;
         }
     }
 
+    public void putPCMData(byte[] buffer, int size) {
+        // 这里需要向音频的编码器中进行塞入数据
+        if (mAudioEncodecThread != null && !mAudioEncodecThread.isExit && buffer != null && size > 0) {
+            int inputBufferIndex = mAudioEncodec.dequeueInputBuffer(0);
+            if (inputBufferIndex >= 0) {
+                ByteBuffer inputBuffer = mAudioEncodec.getInputBuffer(inputBufferIndex);
+                inputBuffer.clear();
+                inputBuffer.put(buffer);
+                long pts = getAudioPts(size, sampleRate);
+                mAudioEncodec.queueInputBuffer(inputBufferIndex, 0, size, pts, 0);
+            }
 
+        }
+    }
 
 
     static class WolfEGLMediaThread extends Thread {
@@ -291,23 +355,28 @@ public abstract class WolfBaseMediaEncoder {
                 int outputBufferIndex = mMediaCodec.dequeueOutputBuffer(mVideoBufferInfo, 0);
                 if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
                     mVideoTrackIndex = mMediaMuxer.addTrack(mMediaCodec.getOutputFormat());
-                    mMediaMuxer.start();
+                    if (encoder.get().mAudioEncodecThread.audioTrackIndex != -1) {
+                        mMediaMuxer.start();
+                        encoder.get().encodecStart = true;
+                    }
                 } else {
                     while (outputBufferIndex >= 0) { // 可能有很多的数据轨道
-                        ByteBuffer outputBuffer = mMediaCodec.getOutputBuffers()[outputBufferIndex];
+                        if (encoder.get().encodecStart) {
+                            ByteBuffer outputBuffer = mMediaCodec.getOutputBuffers()[outputBufferIndex];
 //                        ByteBuffer outputBuffer = mMediaCodec.getOutputBuffer(outputBufferIndex);
-                        outputBuffer.position(mVideoBufferInfo.offset);
-                        outputBuffer.limit(mVideoBufferInfo.offset + mVideoBufferInfo.size);
+                            outputBuffer.position(mVideoBufferInfo.offset);
+                            outputBuffer.limit(mVideoBufferInfo.offset + mVideoBufferInfo.size);
 
-                        if (pts == 0) {
-                            pts = mVideoBufferInfo.presentationTimeUs;// 等于当前的pts值，不要使用默认的，太大
-                        }
-                        mVideoBufferInfo.presentationTimeUs = mVideoBufferInfo.presentationTimeUs - pts;// 这样pts会是从0开始
+                            if (pts == 0) {
+                                pts = mVideoBufferInfo.presentationTimeUs;// 等于当前的pts值，不要使用默认的，太大
+                            }
+                            mVideoBufferInfo.presentationTimeUs = mVideoBufferInfo.presentationTimeUs - pts;// 这样pts会是从0开始
 
-                        // 开始进行编码写入文件
-                        mMediaMuxer.writeSampleData(mVideoTrackIndex, outputBuffer, mVideoBufferInfo);
-                        if (encoder.get().mOnMediaInfoListener != null) {
-                            encoder.get().mOnMediaInfoListener.onMediaTime((int) (mVideoBufferInfo.presentationTimeUs / 1000000));
+                            // 开始进行编码写入文件
+                            mMediaMuxer.writeSampleData(mVideoTrackIndex, outputBuffer, mVideoBufferInfo);
+                            if (encoder.get().mOnMediaInfoListener != null) {
+                                encoder.get().mOnMediaInfoListener.onMediaTime((int) (mVideoBufferInfo.presentationTimeUs / 1000000));
+                            }
                         }
                         mMediaCodec.releaseOutputBuffer(outputBufferIndex, false);
                         outputBufferIndex = mMediaCodec.dequeueOutputBuffer(mVideoBufferInfo, 0); // 开始下一个轨道处理
@@ -322,6 +391,91 @@ public abstract class WolfBaseMediaEncoder {
         }
 
     }
+
+    static class AudioEncodecThread extends Thread {
+        private WeakReference<WolfBaseMediaEncoder> encoder;
+        private boolean isExit;
+
+        private MediaCodec audioEncodec;
+        private MediaCodec.BufferInfo mAudioBufferInfo;
+        private MediaMuxer mMediaMuxer;
+
+        private int audioTrackIndex = -1;
+        /**
+         * pts 很重要，这个和音视频同步相关。
+         */
+        private long pts;
+
+        public AudioEncodecThread(WeakReference<WolfBaseMediaEncoder> encoder) {
+            this.encoder = encoder;
+            this.audioEncodec = encoder.get().mAudioEncodec;
+            this.mAudioBufferInfo = encoder.get().mAudioBufferInfo;
+            this.mMediaMuxer = encoder.get().mMediaMuxer;
+            audioTrackIndex = -1;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            pts = 0;
+            isExit = false;
+            audioEncodec.start();
+            while (true) {
+                if (isExit) {
+                    // 结束编码
+                    audioEncodec.stop();
+                    audioEncodec.release();
+                    audioEncodec = null;
+                    encoder.get().audioExit = true; // 音频编码和视频编码要相互关联，同时进行退出
+                    if (encoder.get().videoExit) {
+                        mMediaMuxer.stop();
+                        mMediaMuxer.release();
+                        mMediaMuxer = null;
+                    }
+                    break;
+                }
+                // 然后就开始了编码
+                int outputBufferIndex = audioEncodec.dequeueOutputBuffer(mAudioBufferInfo, 0);
+                if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    // 设置编码混合器进行开始工作
+                    if (mMediaMuxer != null) {
+                        audioTrackIndex = mMediaMuxer.addTrack(audioEncodec.getOutputFormat());
+                        if (encoder.get().mVideoEncodecThread.mVideoTrackIndex != -1) { // 视频已经准备好了
+                            mMediaMuxer.start();
+                            encoder.get().encodecStart = true;
+                        }
+                    }
+                } else {
+                    while (outputBufferIndex >= 0) {
+                        if (encoder.get().encodecStart) { // 开始进行编码写入
+                            ByteBuffer outputBuffer = audioEncodec.getOutputBuffer(outputBufferIndex);
+                            outputBuffer.position(mAudioBufferInfo.offset);
+                            outputBuffer.limit(mAudioBufferInfo.offset + mAudioBufferInfo.size);
+                            if (pts == 0) {
+                                pts = mAudioBufferInfo.presentationTimeUs;
+                            }
+                            mAudioBufferInfo.presentationTimeUs = mAudioBufferInfo.presentationTimeUs - pts;
+                            mMediaMuxer.writeSampleData(audioTrackIndex, outputBuffer, mAudioBufferInfo);
+                        }
+                        audioEncodec.releaseOutputBuffer(outputBufferIndex, false);
+                        outputBufferIndex = audioEncodec.dequeueOutputBuffer(mAudioBufferInfo, 0);
+                    }
+                }
+            }
+        }
+
+        public void exit() {
+            isExit = true;
+        }
+
+    }
+
+    private long getAudioPts(int size, int sampleRate) {
+        // 大小 = 采样率 * 采样大小(16bit) * 声道数
+        audioPts += (long) (1.0 * size / (sampleRate * 2 * 2) * 1000000.0);
+        return audioPts;
+    }
+
 
     public interface OnMediaInfoListener {
         void onMediaTime(int time);
